a08f41ea2e7cf22799b5f16a0bdfb18f6e5ea1a4
Marciele Adivincula <marcieleadivincula@gmail.com>
2021-06-08 23:55:45 -0300

    Update login.spec.js

--- login.spec.js
+++ login.spec.js
@@ -8,15 +8,17 @@ const users = {
   5: { user: "fabio@gmail.com", senha: "159" },
 };
 
-const userExpected = {
-  1: { user: "fernanda@gmail.com", senha: "123" },
-};
-
 describe("Efetuar Login", () => {
   // Aqui vão os testes que descrevem o comportamento de `login()`
   describe("Preencher os campos Usuário e senha com valores válidos", () => {
+    it("Deveria retornar true", function () {
+      assert.equal(login(users[1].user, users[1].senha), true);
+    });
+  });
+
+  describe("Preencher os campos Usuário e senha com valores inválidos", () => {
     it("Deveria retornar false", function () {
-      assert.equal(users[1], userExpected[1], "Comparando usuários");
+      assert.equal(login(users[2].user, users[2].senha), false);
     });
   });
 });

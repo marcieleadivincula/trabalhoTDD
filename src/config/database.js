76aa97eeda0038b906b723135538f7fb496a6b53
marcieleadivincula <marcieleadivincula@gmail.com>
2021-06-29 09:50:10 -0300

    Create database.js

---
+++ src/config/database.js
@@ -0,0 +1,14 @@
+module.exports = {
+  host: "127.0.0.1",
+  username: "root",
+  password: "#Almeid@21",
+  database: "nodeauth",
+  dialect:"mysql2"
+  operatorsAliases: false,
+  logging: false,
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true,
+  },
+}
